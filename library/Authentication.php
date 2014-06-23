2e388b31d9d4ded8ca99424cdbf9d3263898f302
Traven <traven.west@gmail.com>
2014-06-22 22:53:54 -0700

    Create Authentication.php

---
+++ library/Authentication.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ *   Package:  Core Application
+ *   Authors:  Traven, Todd
+ *   Version:  1.o.0
+**/
+
+/**
+ *   Base QuadCMS Authentication Class  
+ *   Sets up the environment as necessary and acts as the registry
+ *   for QuadCMS and the Core Application.
+**/
+
+class Authentication {
+}
